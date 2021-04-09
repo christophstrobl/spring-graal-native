0aaadc2e1f00acb543ab7f4f443d30759379d972
Christoph Strobl <cstrobl@vmware.com>
2021-04-09 13:01:46 +0200

    Add jpa component processor.

---
+++ spring-native-configuration/src/main/java/org/springframework/data/JpaComponentProcessor.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data;
+
+import java.util.List;
+
+import org.springframework.nativex.hint.AccessBits;
+import org.springframework.nativex.type.ComponentProcessor;
+import org.springframework.nativex.type.NativeContext;
+import org.springframework.nativex.type.Type;
+
+/**
+ * @author Christoph Strobl
+ */
+public class JpaComponentProcessor implements ComponentProcessor {
+
+	private final DomainTypeProcessor domainTypeProcessor = new DomainTypeProcessor(
+
+			(type, nativeContext) -> {
+				if (nativeContext.hasReflectionConfigFor(type)) {
+					return false;
+				}
+				return true;
+			},
+			this::registerTypeInConfiguration,
+			this::registerAnnotationInConfiguration
+	);
+
+
+	@Override
+	public boolean handle(NativeContext imageContext, String componentType, List<String> classifiers) {
+
+		Type type = imageContext.getTypeSystem().resolveName(componentType);
+
+		return type.getAnnotations() //
+				.stream() //
+				.filter(it -> it.getDottedName().contains("javax.persistence"))
+				.count() > 0;
+	}
+
+	@Override
+	public void process(NativeContext imageContext, String componentType, List<String> classifiers) {
+
+		Type domainType = imageContext.getTypeSystem().resolveName(componentType);
+		domainTypeProcessor.process(domainType, imageContext);
+	}
+
+	private void registerAnnotationInConfiguration(Type annotation, NativeContext context) {
+		context.addReflectiveAccess(annotation.getDottedName(), AccessBits.ANNOTATION);
+	}
+
+	private void registerTypeInConfiguration(Type type, NativeContext context) {
+
+		// TODO: be explicit here - eg. enums, final fields, ...
+
+		context.addReflectiveAccess(type.getDottedName(), AccessBits.FULL_REFLECTION);
+	}
+}
